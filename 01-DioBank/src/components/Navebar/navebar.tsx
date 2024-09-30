c860297953bc91901647f54b0e69c2464b161921
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-09-29 21:13:20 -0300

    feat: :sparkles: Added Navebar
    -	new file:   src/components/Navebar/navebar.css
    -	new file:   src/components/Navebar/navebar.tsx

---
+++ 01-DioBank/src/components/Navebar/navebar.tsx
@@ -0,0 +1,9 @@
+import './navebar.css'
+export function Navebar(){
+  return (<div className="navebar">
+    <input type="button" value="Home" />
+    <input type="button" value="Conta" />
+    <input type="button" value="Transferências" />
+    <input type="button" value="Suporte" />
+  </div>)
+}

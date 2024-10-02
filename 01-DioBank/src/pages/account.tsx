1ddc67cb3b24b9b13249eeea6d1ebc564a8e76cf
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-10-02 11:08:07 -0300

    feat: :sparkles: Created folder "pages"
    - Inproved structure of pages
    - Created file "home"
    - Created file "account"

---
+++ 01-DioBank/src/pages/account.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header/header"
+import { Navbar } from "../components/Navbar/navbar"
+import { Footer } from "../components/Footer/footer"
+
+export const Account = () => {
+  return (
+  <>
+    <Header />
+    <Navbar />
+    <h1>Login realizado com sucesso!</h1>
+    <Footer />
+  </>
+)} 
---
+++ 01-DioBank/src/pages/home.tsx
@@ -0,0 +1,14 @@
+import { Header } from "../components/Header/header"
+import { Navbar } from "../components/Navbar/navbar"
+import { Login } from "../components/Main/login-main"
+import { Footer } from "../components/Footer/footer"
+
+export const Home = () => {
+  return (
+  <>
+    <Header />
+    <Navbar />
+    <Login />
+    <Footer />
+  </>
+)} 

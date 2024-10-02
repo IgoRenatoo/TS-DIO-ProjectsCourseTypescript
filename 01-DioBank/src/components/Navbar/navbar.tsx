351255c0bc973d4a850cca517343cbd581c8e5c7
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-10-02 11:01:02 -0300

    feat: :art: Improved code structure
    - Improved indentation of code

--- 01-DioBank/src/components/Navbar/navbar.tsx
+++ 01-DioBank/src/components/Navbar/navbar.tsx
@@ -1,9 +1,12 @@
 import './navbar.css'
+
 export function Navbar(){
-  return (<div className="navbar">
-    <input type="button" value="Home" />
-    <input type="button" value="Conta" />
-    <input type="button" value="Transferências" />
-    <input type="button" value="Suporte" />
-  </div>)
+  return (
+    <div className="navbar">
+      <input type="button" value="Home" />
+      <input type="button" value="Conta" />
+      <input type="button" value="Transferências" />
+      <input type="button" value="Suporte" />
+    </div>
+  )
 }

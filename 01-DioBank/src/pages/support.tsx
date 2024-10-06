5f9b682668e168769c102a08ab4cbafc3e207bd3
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-10-06 15:53:40 -0300

    feat: :sparkles: Created page of Support

---
+++ 01-DioBank/src/pages/support.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header/header"
+import { Navbar } from "../components/Navbar/navbar"
+import { Footer } from "../components/Footer/footer"
+
+export const Support = () => {
+  return (
+  <>
+    <Header />
+    <Navbar />
+    <main></main>
+    <Footer />
+  </>
+)} 

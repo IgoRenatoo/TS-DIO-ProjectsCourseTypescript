d6d0780128f961ef71213663b60d97bf83134384
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-10-06 15:54:46 -0300

    feat: :sparkles: Created page of Transfer

---
+++ 01-DioBank/src/pages/transfer.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header/header"
+import { Navbar } from "../components/Navbar/navbar"
+import { Footer } from "../components/Footer/footer"
+
+export const Transfer = () => {
+  return (
+  <>
+    <Header />
+    <Navbar />
+    <main></main>
+    <Footer />
+  </>
+)} 
